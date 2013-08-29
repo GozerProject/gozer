430819a00a660134f277382434ee192d117bb6c1
sebastien <seb.brousse@gmail.com>
2013-08-29 20:25:03 +0200

    Correct unit test

--- src/test/java/org/gozer/services/DependenciesServiceTest.java
+++ src/test/java/org/gozer/services/DependenciesServiceTest.java
@@ -12,14 +12,13 @@ public class DependenciesServiceTest {
 
     @Test
     public void should_resolve_maven_dependency() {
-        DependenciesService service = new DependenciesService();
+        DependenciesService service = new DependenciesService("target/repositories");
         Dependency dependency = aDependency().withGroupId("org.apache.camel")
                                             .withArtifactId("camel-core")
                                             .withVersion("2.10.4")
                                             .build();
         service.resolve(dependency);
 
-        // TODO use an injected properties
         assertThat(new File("target/repositories/org/apache/camel/camel-core/2.10.4/", "camel-core-2.10.4.jar")).exists();
     }
 }
